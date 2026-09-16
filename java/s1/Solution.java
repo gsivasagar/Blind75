dca41d42179729b180f08f7a5841373828179f3e
siva sagar <sivasagar1909@gmail.com>
2026-09-17 01:21:28 +0530

    Initial commit: Add Two Sum solution

---
+++ java/s1/Solution.java
@@ -0,0 +1,32 @@
+package Java.s1;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        int[] idx = new int[2];
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < n; i++) {
+            int rem = target - nums[i];
+            if (map.containsKey(rem)) {
+                int index = map.get(rem);
+                if (index == i) {
+                    continue;
+                }
+                idx[0] = i;
+                idx[1] = map.get(rem);
+            }
+        }
+        return idx;
+    }
+}
+
+//Time:- O(n), Space:- O(n)
+//nums = [2,7,11,15], target = 9
+//[0,1]
+//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
+//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
